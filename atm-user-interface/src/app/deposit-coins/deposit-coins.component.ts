73c3f97fd210647a4929e4c6e948133ef3aba793
Liza Feng <liza.feng05@myhunter.cuny.edu>
2025-06-27 16:01:59 -0400

    adding the basic user interface look and components

---
+++ atm-user-interface/src/app/deposit-coins/deposit-coins.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-deposit-coins',
+  imports: [],
+  templateUrl: './deposit-coins.component.html',
+  styleUrl: './deposit-coins.component.css'
+})
+export class DepositCoinsComponent {
+
+}
---
+++ atm-user-interface/src/app/deposit-notes/deposit-notes.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-deposit-notes',
+  imports: [],
+  templateUrl: './deposit-notes.component.html',
+  styleUrl: './deposit-notes.component.css'
+})
+export class DepositNotesComponent {
+
+}
